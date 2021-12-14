e4199ebe8f8a3e1d2a7636a0f3cedb9af3322fd1
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-14 13:55:20 -0600

    Create an object for projects & access properties

---
+++ scripts/projects-modal.js
@@ -0,0 +1,13 @@
+const projects_section = document.querySelector('.projects');
+const projects = document.querySelectorAll('.projects > div'); //one, two, three
+
+const projectList = new Array();
+for (let i = 1; i <= 6; i++){
+    projectList[i] = new Object();
+    projectList[i].title = projects[i].querySelector('.project-summary > h2');
+    projectList[i].technologies = projects[i].querySelector('.technologies');
+    projectList[i].thumbnail = projects[i].querySelector('.thumbnail');
+}
+
+
+
