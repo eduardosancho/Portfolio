6ce63eed8abd6d6110bbd21082a3ae96bde19e39
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-13 20:40:43 -0600

    Make navbar disappear auto when you click a link

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,17 +1,19 @@
 const menu = document.querySelector('.menu');
 const open = document.querySelector('.open');
 const close = document.querySelector('li.close');
+const sectionLinks = document.querySelectorAll('.menu-link');
 
 
 open.addEventListener('click', show);
-close.addEventListener('click', hide)
+close.addEventListener('click', hide);
+sectionLinks.forEach(item => {
+    item.addEventListener('click', hide)
+});
 
 function show() {
     menu.style.top = "0";
-    console.log("show allllllll");
 }
 
 function hide() {
     menu.style.top = "-100%";
-    console.log("hideeeeeeeeeee");
 }
