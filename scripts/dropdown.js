b826790cbc1d2f2250268312a08e2425b43db764
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-13 20:46:52 -0600

    Ran linter checkers

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -3,17 +3,16 @@ const open = document.querySelector('.open');
 const close = document.querySelector('li.close');
 const sectionLinks = document.querySelectorAll('.menu-link');
 
-
-open.addEventListener('click', show);
-close.addEventListener('click', hide);
-sectionLinks.forEach(item => {
-    item.addEventListener('click', hide)
-});
-
 function show() {
-    menu.style.top = "0";
+  menu.style.top = '0';
 }
 
 function hide() {
-    menu.style.top = "-100%";
+  menu.style.top = '-100%';
 }
+
+open.addEventListener('click', show);
+close.addEventListener('click', hide);
+sectionLinks.forEach((item) => {
+  item.addEventListener('click', hide);
+});
