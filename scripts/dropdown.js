591c0628042288766518c0d85ad3910a7ddc0c6c
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-14 12:53:23 -0600

    Rename show and hide functions

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -3,16 +3,16 @@ const open = document.querySelector('.open');
 const close = document.querySelector('li.close');
 const sectionLinks = document.querySelectorAll('.menu-link');
 
-function show() {
+function showNav() {
   menu.style.top = '0';
 }
 
-function hide() {
+function hideNav() {
   menu.style.top = '-100%';
 }
 
-open.addEventListener('click', show);
-close.addEventListener('click', hide);
+open.addEventListener('click', showNav);
+close.addEventListener('click', hideNav);
 sectionLinks.forEach((item) => {
   item.addEventListener('click', hide);
 });
