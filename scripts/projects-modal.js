6b378a6fb9910d9652e9096b3b64075322b3c413
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-14 22:55:58 -0600

    Fix ESLint suggestions

--- scripts/projects-modal.js
+++ scripts/projects-modal.js
@@ -1,59 +1,34 @@
-const projects_section = document.querySelector('.projects');  //Entire section
-const projects = document.querySelectorAll('.projects > div'); //one, two, three
+/* const projects_section = document.querySelector('.projects'); // Entire section */
+const projects = document.querySelectorAll('.projects > div'); // one, two, three
 
-const projectList = new Array();  //Extract info to recycle
-for (let i = 1; i <= 6; i++) {
-    projectList[i] = new Object();
-    projectList[i].title = projects[i].querySelector('.project-summary > h2');
-    projectList[i].technologies = projects[i].querySelector('.technologies');
-    projectList[i].thumbnail = projects[i].querySelector('.thumbnail');
+const projectList = []; // Extract info to recycle
+for (let i = 1; i <= 6; i += 1) {
+  projectList[i] = {};
+  projectList[i].title = projects[i].querySelector('.project-summary > h2');
+  projectList[i].technologies = projects[i].querySelector('.technologies');
+  projectList[i].thumbnail = projects[i].querySelector('.thumbnail');
 }
 
-const project_button = document.querySelector('.see-project');
-project_button.addEventListener('click', openPopup); //Click to show
-function openPopup() {
-    document.body.appendChild(projectOne);
-    projectOne.style.top = '0';
-    projectOne.appendChild(projectList[1].title.cloneNode(true));
-    projectOne.appendChild(projectList[1].technologies.cloneNode(true));
-    projectOne.appendChild(projectList[1].thumbnail.cloneNode(true));
-    projectOne.appendChild(projectOneDescription);
-    projectOne.appendChild(projectOneSeeLive);
-    projectOne.appendChild(projectOneSeeSource);
-    document.head.appendChild(style);/* 
-    document.querySelector('.one .project-summary').removeChild(document.querySelector('.one .see-project'));
- */
-    const close_project = document.querySelector('.close-project');
-    close_project.addEventListener('click', closePopup); //Click to close
-    function closePopup() {
-        projectOne.style.top = '-100%';
-        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup h2'));
-        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .technologies'));
-        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .thumbnail'));
-    };
-};
-
-let projectOne = document.createElement('div'); //Create popup div
+const projectOne = document.createElement('div'); // Create popup div
 projectOne.className = 'show-popup';
 projectOne.innerHTML = `
 <div class="close-project"><img src="images/times.svg" alt="Close project icon"></div>
 `;
 
-let projectOneDescription = document.createElement('p');
+const projectOneDescription = document.createElement('p');
 projectOneDescription.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.';
-let projectOneSeeLive = document.createElement('div');
+const projectOneSeeLive = document.createElement('div');
 projectOneSeeLive.innerHTML = `
 <button class="see-live" type="button">
     See live<img src="images/ic_download.svg" alt="download-icon">
 </button>
 `;
-let projectOneSeeSource = document.createElement('div');
+const projectOneSeeSource = document.createElement('div');
 projectOneSeeSource.innerHTML = `
 <button class="see-source" type="button">
     See source<img src="images/ic_github.svg" alt="github">
 </button>
 `;
-
 
 const style = document.createElement('style');
 style.innerHTML = `
@@ -90,5 +65,25 @@ style.innerHTML = `
     }
     `;
 
+function openPopup() {
+  document.body.appendChild(projectOne);
+  projectOne.style.top = '0';
+  projectOne.appendChild(projectList[1].title.cloneNode(true));
+  projectOne.appendChild(projectList[1].technologies.cloneNode(true));
+  projectOne.appendChild(projectList[1].thumbnail.cloneNode(true));
+  projectOne.appendChild(projectOneDescription);
+  projectOne.appendChild(projectOneSeeLive);
+  projectOne.appendChild(projectOneSeeSource);
+  document.head.appendChild(style);
+  function closePopup() {
+    projectOne.style.top = '-100%';
+    document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup h2'));
+    document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .technologies'));
+    document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .thumbnail'));
+  }
+  const closeProject = document.querySelector('.close-project');
+  closeProject.addEventListener('click', closePopup); // Click to close
+}
 
-
+const projectBtn = document.querySelector('.see-project');
+projectBtn.addEventListener('click', openPopup); // Click to show
