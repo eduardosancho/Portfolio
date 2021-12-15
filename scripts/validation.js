d7a8e8443516a98e76d4168135d828147461d541
david-lafontant <lafontant.david@gmail.com>
2021-12-15 14:49:19 -0500

    Create the validation script and link it to index

---
+++ scripts/validation.js
@@ -0,0 +1,12 @@
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
