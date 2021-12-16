37ed22e837cbdf98c90d625812f1d988d2424946
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-15 22:09:47 -0600

    Fix linter suggestions.

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,62 +1,62 @@
 // show a message with a type of the input
 function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
 
 function showSuccess(input) {
-	return showMessage(input, "", true);
+  return showMessage(input, '', true);
 }
 
 function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
 }
 
 function validateEmail(input, requiredMsg, invalidMsg, capitalsInEmail) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex = /^(([^<>()\\.,;:\s@"]+(\.[^<>()\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	} else if (email != email.toLowerCase()) {
-		return showError(input, capitalsInEmail);
-	}
-	return true;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  } if (email !== email.toLowerCase()) {
+    return showError(input, capitalsInEmail);
+  }
+  return true;
 }
 
-const form = document.querySelector("#signup");
+const form = document.querySelector('#signup');
 
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
-const EMAIL_HASCAPITALS = "Please make sure your email is only in lower case";
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter a correct email address format';
+const EMAIL_HASCAPITALS = 'Please make sure your email is only in lower case';
 
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
+form.addEventListener('submit', function (event) {
+  // stop form submission
+  event.preventDefault();
 
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID, EMAIL_HASCAPITALS);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		this.submit();
-	}
+  // validate the form
+  const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(form.elements.email, EMAIL_REQUIRED, EMAIL_INVALID,
+    EMAIL_HASCAPITALS);
+  // if valid, submit the form.
+  if (nameValid && emailValid) {
+    this.submit();
+  }
 });
