092357d94e30895e1157fd850354cf9e6de3217b
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-16 21:07:57 -0600

    Store form data as an object

--- scripts/store-data.js
+++ scripts/store-data.js
@@ -1,6 +1,16 @@
 const name = document.querySelector("#name");
 const name = document.querySelector("#email");
 
+function storeData() {
+  const contactFormData = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('msg').value
+  };
+
+  localStorage.setItem('contactInfo', JSON.stringify(contactFormData));
+}
+
 function displayData (){
   console.log(name);
 }
