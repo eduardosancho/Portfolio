233709b93d9ba6026c3b35999313c7d749ae8df8
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-17 02:41:59 -0600

    Add a project modal for all projects

--- scripts/projects-modal.js
+++ scripts/projects-modal.js
@@ -1,4 +1,4 @@
-const projects = document.querySelectorAll('.projects > div'); // find all projects in the DOM
+const projects = document.querySelectorAll('.projects > div');
 
 const projectList = []; // Extract info to reuse
 for (let i = 1; i <= 6; i += 1) {
@@ -8,11 +8,12 @@ for (let i = 1; i <= 6; i += 1) {
   projectList[i].thumbnail = projects[i].querySelector('.thumbnail');
 }
 
-const projectPopUp = document.createElement('div'); // Create popup div
+const projectPopUp = document.createElement('div');
 projectPopUp.className = 'show-popup';
-projectPopUp.innerHTML = `
-<div class="popUpTop"><img class="close-project" src="images/times.svg" alt="Close project icon"></div>
-`;
+
+const popUpTop = document.createElement('div');
+popUpTop.className = 'popUpTop';
+popUpTop.innerHTML = '<img class="close-project" src="images/times.svg" alt="Close project icon">';
 
 const projectOneDescription = document.createElement('p');
 projectOneDescription.textContent = 'Mobile app that displays updated stock data from Financial Modelig Prep API.';
@@ -28,65 +29,132 @@ projectOneSeeLiveSource.innerHTML = `
   </button>
 `;
 
-const projectDescription = document.createElement('p');
-projectDescription.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.';
+const projectTwoDescription = document.createElement('p');
+projectTwoDescription.textContent = 'Track the deathcount of players from the videogame Elden Ring. Users can input their deathcount and see other people\'s';
 
-const projectSeeLiveSource = document.createElement('div');
-projectSeeLiveSource.className = 'projectSee';
-projectSeeLiveSource.innerHTML = `
-<button class="see-live" type="button" onclick="window.location.href='https://eduardosancho.github.io/setup_and_mobile_version_skeleton/'">
-<p>See live</p><img src="images/ic_link.svg" alt="download-icon">
-</button>
-<button class="see-source" type="button" onclick="window.location.href='https://github.com/eduardosancho/setup_and_mobile_version_skeleton'">
+const projectTwoSeeLiveSource = document.createElement('div');
+projectTwoSeeLiveSource.className = 'projectSee';
+projectTwoSeeLiveSource.innerHTML = `
+  <button class="see-live" type="button" onclick="window.open('https://eduardosancho.github.io/Leaderboard/dist/index.html'); return false;">
+    <p>See live</p><img src="images/ic_link.svg" alt="download-icon">
+  </button>
+  <button class="see-source" type="button" onclick="window.open('https://github.com/eduardosancho/Leaderboard'); return false;">
     <p>See source</p><img src="images/ic_github_white.svg" alt="github">
-</button>
+  </button>
+`;
+
+const projectThreeDescription = document.createElement('p');
+projectThreeDescription.textContent = 'Web application that showcases Yu-Gi-Oh! cards. You can like and comment cards.';
+
+const projectThreeSeeLiveSource = document.createElement('div');
+projectThreeSeeLiveSource.className = 'projectSee';
+projectThreeSeeLiveSource.innerHTML = `
+  <button class="see-live" type="button" onclick="window.open('https://yothu.github.io/Yu-Gi-Oh--Card-Data-Base/dist/index.html'); return false;">
+    <p>See live</p><img src="images/ic_link.svg" alt="download-icon">
+  </button>
+  <button class="see-source" type="button" onclick="window.open('https://github.com/eduardosancho/Yu-Gi-Oh--Card-Data-Base'); return false;">
+    <p>See source</p><img src="images/ic_github_white.svg" alt="github">
+  </button>
+`;
+
+const projectFourDescription = document.createElement('p');
+projectFourDescription.textContent = 'A hotel reservation app displays all the existing hotels and details of each hotel, allowing the user to reserve the hotel they want.';
+
+const projectFourSeeLiveSource = document.createElement('div');
+projectFourSeeLiveSource.className = 'projectSee';
+projectFourSeeLiveSource.innerHTML = `
+  <button class="see-live" type="button" onclick="window.open('https://reservify.netlify.app/'); return false;">
+    <p>See live</p><img src="images/ic_link.svg" alt="download-icon">
+  </button>
+  <button class="see-source" type="button" onclick="window.open('https://github.com/eduardosancho/reservify-front-end'); return false;">
+    <p>See source</p><img src="images/ic_github_white.svg" alt="github">
+  </button>
+`;
+
+const projectFiveDescription = document.createElement('p');
+projectFiveDescription.textContent = 'Desktop app to register own recipes and look at public recipes aswell.';
+
+const projectFiveSeeLiveSource = document.createElement('div');
+projectFiveSeeLiveSource.className = 'projectSee';
+projectFiveSeeLiveSource.innerHTML = `
+  <button class="see-live" type="button" onclick="window.open('https://infinite-stream-75498.herokuapp.com/'); return false;">
+    <p>See live</p><img src="images/ic_link.svg" alt="download-icon">
+  </button>
+  <button class="see-source" type="button" onclick="window.open('https://github.com/eduardosancho/Recipe-App-Capstone'); return false;">
+    <p>See source</p><img src="images/ic_github_white.svg" alt="github">
+  </button>
+`;
+
+const projectSixDescription = document.createElement('p');
+projectSixDescription.textContent = 'Mobile app to organize spending and group transactions into categories.';
+
+const projectSixSeeLiveSource = document.createElement('div');
+projectSixSeeLiveSource.className = 'projectSee';
+projectSixSeeLiveSource.innerHTML = `
+  <button class="see-live" type="button" onclick="window.open('https://rocky-cliffs-67926.herokuapp.com/'); return false;">
+    <p>See live</p><img src="images/ic_link.svg" alt="download-icon">
+  </button>
+  <button class="see-source" type="button" onclick="window.open('https://github.com/eduardosancho/budget'); return false;">
+    <p>See source</p><img src="images/ic_github_white.svg" alt="github">
+  </button>
 `;
 
 // eslint-disable-next-line no-unused-vars
 function displayProject(num) {
   projectPopUp.style.top = '0';
+  projectPopUp.appendChild(popUpTop);
   projectPopUp.querySelector('div').appendChild(projectList[num].title.cloneNode(true));
   projectPopUp.appendChild(projectList[num].technologies.cloneNode(true));
   projectPopUp.appendChild(projectList[num].thumbnail.cloneNode(true));
   switch (num) {
     case 1:
-      // add class two to thumbnail element in popup
       projectPopUp.querySelector('.thumbnail').classList.add('one');
-      projectPopUp.appendChild(projectOneDescription); // Add description
+      projectPopUp.appendChild(projectOneDescription);
       projectPopUp.appendChild(projectOneSeeLiveSource);
       break;
     case 2:
       projectPopUp.querySelector('.thumbnail').classList.add('two');
+      projectPopUp.appendChild(projectTwoDescription);
+      projectPopUp.appendChild(projectTwoSeeLiveSource);
       break;
     case 3:
       projectPopUp.querySelector('.thumbnail').classList.add('three');
+      projectPopUp.appendChild(projectThreeDescription);
+      projectPopUp.appendChild(projectThreeSeeLiveSource);
       break;
     case 4:
       projectPopUp.querySelector('.thumbnail').classList.add('four');
+      projectPopUp.appendChild(projectFourDescription);
+      projectPopUp.appendChild(projectFourSeeLiveSource);
       break;
     case 5:
       projectPopUp.querySelector('.thumbnail').classList.add('five');
+      projectPopUp.appendChild(projectFiveDescription);
+      projectPopUp.appendChild(projectFiveSeeLiveSource);
       break;
     case 6:
       projectPopUp.querySelector('.thumbnail').classList.add('six');
+      projectPopUp.appendChild(projectSixDescription);
+      projectPopUp.appendChild(projectSixSeeLiveSource);
       break;
     default:
       break;
   }
-  projectPopUp.appendChild(projectDescription);
-  projectPopUp.appendChild(projectSeeLiveSource);
   function closePopup() {
     projectPopUp.style.top = '-100%';
     setTimeout(() => {
-      document.querySelector('.show-popup > .popUpTop').removeChild(document.querySelector('.popUpTop h2'));
-      document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .technologies'));
-      document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .thumbnail'));
+      if (document.querySelector('.show-popup').textContent !== '') {
+        document.querySelector('.show-popup > .popUpTop').removeChild(document.querySelector('.popUpTop > h2'));
+        document.querySelector('.show-popup').removeChild(document.querySelector('.popUpTop'));
+        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .technologies'));
+        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .thumbnail'));
+        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup p'));
+        document.querySelector('.show-popup').removeChild(document.querySelector('.show-popup .projectSee'));
+      }
     }, 1000);
   }
-  const closeProject = document.querySelectorAll('.close-project');
-  closeProject.forEach((item) => {
-    item.addEventListener('click', closePopup); // Click to close
-  });
+  const closeProject = document.querySelector('.close-project');
+  closeProject.addEventListener('click', closePopup);
 }
 
 document.body.appendChild(projectPopUp);
